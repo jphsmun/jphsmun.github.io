3bfa1a42b66cc4f7425360b2fe0c7283712b7130
A8H1K <abhikchoudhary2007@gmail.com>
2022-01-27 21:06:47 +0530

    fixed timer and comm cards

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   // Unix timestamp (in seconds) to count down to
-  var twoDaysFromNow = (new Date("Feb 18, 2022").getTime() / 1000) + (86400 * 2) + 1;
+  var TimeLeft = (new Date("Feb 18, 2022").getTime() / 1000) + (86400 * 2) + 1;
 
   // Set up FlipDown
-  var flipdown = new FlipDown(twoDaysFromNow)
+  var flipdown = new FlipDown(1645122601, {
+    theme: 'light',
+    countdown: true,})
 
     // Start the countdown
     .start()
@@ -13,14 +15,6 @@ document.addEventListener('DOMContentLoaded', () => {
     .ifEnded(() => {
       console.log('The JPHS MUN 2022 has Started');
     });
-
-  // Toggle theme
-  var interval = setInterval(() => {
-    let body = document.body;
-    body.classList.toggle('light-theme');
-    body.querySelector('#flipdown').classList.toggle('flipdown__theme-dark');
-    body.querySelector('#flipdown').classList.toggle('flipdown__theme-light');
-  }, 5000);
 
   // Show version number
   var ver = document.getElementById('ver');
