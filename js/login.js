b63d7bc687d23e28f4b8125679bf0569378f9060
A8H1K <abhikchoudhary2007@gmail.com>
2022-02-17 19:20:21 +0530

    added error 404 auth

---
+++ js/login.js
@@ -0,0 +1,20 @@
+var objPeople = [
+	{ // Object @ 0 index
+		username: "Error-404",
+		password: "Muahahaha"
+	},
+]
+
+function getInfo() {
+	var username = document.getElementById('username').value
+	var password = document.getElementById('password').value
+
+	for(var i = 0; i < objPeople.length; i++) {
+		// check is user input matches username and password of a current index of the objPeople array
+		if(username == objPeople[i].username && password == objPeople[i].password) {
+			window.location.replace("dashboard.html");			// stop the function if this is found to be true
+			return
+		}
+	}
+	console.log("incorrect username or password")
+}
