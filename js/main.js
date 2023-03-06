ffd619094cd6c4e7996d40929847b8cde24aef99
jphsmun <96812849+jphsmun@users.noreply.github.com>
2023-03-06 11:50:56 +0530

    Revised dates

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   // Unix timestamp (in seconds) to count down to
-  var TimeLeft = (new Date("Feb 18, 2022").getTime() / 1000) + (86400 * 2) + 1;
+  var TimeLeft = (new Date("May 5, 2023").getTime() / 1000) + (86400 * 2) + 1;
 
   // Set up FlipDown
   var flipdown = new FlipDown(1645122601, {
